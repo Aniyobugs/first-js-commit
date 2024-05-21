218f2bd39b7ab991b1645e93645563daa30394da
ANANDHU KRISHNA CS <150225901+Aniyobugs@users.noreply.github.com>
2024-05-21 14:49:29 +0530

    Add files via upload

---
+++ ANIYO JS/script.js
@@ -0,0 +1,71 @@
+// // postfix increment
+// let a=1;
+// let b=a++;
+// console.log(a);
+// console.log(b);
+
+// // prefix increment
+// let c=1;
+// let d=++c;
+// console.log(c);
+// console.log(d);  
+// let person={
+//     fname:"ANANDHU KRISHNA CS",
+//     address:{
+//           street:"north",
+//           place:"chalakudy",
+//           pin:680307
+//     },
+//     Age:18       
+// };
+
+// console.log(person.fname);
+// console.log(person.Age);
+// console.log(person.address);
+
+
+// var toothbrush=new Object()
+// toothbrush.brand="SENSODYNE";
+// toothbrush.color="BLUE";
+// console.log(toothbrush);
+
+// // concatination
+// let fname="ANANDHU";
+// let lname="KRISHNA";
+// console.log(`${fname} ${lname}`);
+// let m=5;
+// let n=6;
+// console.log(`The sum is ${m+n}`);
+
+// // ARRAY
+// let fruits=["Mango","Apple","Orange",10]
+// console.log(fruits);
+// fruits.push("Peaches");
+// console.log(fruits);
+// console.log(fruits.length);
+// fruits[2]="PINEAPPLE";
+// console.log(fruits);
+// fruits.unshift("pear")
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits)
+
+// Array of Object
+let bsc=[
+    {
+        fname:"Anandhu",
+        age:20,
+    },
+    {
+        fname:"Aniyo",
+        age:18,
+    },
+    {
+        fname:"Bugs",
+        age:16,
+    },
+
+];
+console.log(bsc[1].age);
---
+++ ANIYO JS/script2.js
@@ -0,0 +1,11 @@
+// function add (a,b){
+//     let c=a+b;
+//     console.log(c);
+// }
+// add(1,2);
+
+// function
+function fullname(fname,lname){
+    console.log(`${fname} ${lname}`);
+}
+fullname("Aniyo","Bugs")
